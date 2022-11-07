50d7a702a4b56fab8319e30dc2d80c38286c312b
Emma <emersonpaige5@gmail.com>
2022-11-07 13:03:38 -0500

    sign-up function

--- app/app.js
+++ app/app.js
@@ -1,19 +1,52 @@
 import * as MODEL from "./model.js";
 
 function changeRoute() {
-    let pageID = window.location.hash.replace('#', '');
-    if (pageID == "" || pageID == "home") {
-        MODEL.changePage(pageID);
-    } else {
-        MODEL.changePage(pageID);
-    }
+  let pageID = window.location.hash.replace("#", "");
+  if (pageID == "" || pageID == "home" || pageID == "login") {
+    MODEL.changePage(pageID, initSubmitListener);
+  } else {
+    MODEL.changePage(pageID);
+  }
 }
 
 function initURLListener() {
-    $(window).on('hashchange', changeRoute);
-    changeRoute();
+  $(window).on("hashchange", changeRoute);
+  changeRoute();
+}
+
+// sign-up function
+
+function initSubmitListener() {
+  $("#signup-submit").on("click", function (e) {
+    console.log("submit");
+
+    let fn = $("#signup-fn").val();
+    let ln = $("#signup-ln").val();
+    let em = $("#signup-email").val();
+    let pw = $("#signup-pw").val();
+
+    if (fn == "") {
+      alert("Enter first name.");
+    } else if (ln == "") {
+      alert("Enter last name.");
+    } else if (em == "") {
+      alert("Enter email.");
+    } else if (pw == "") {
+      alert("Enter password.");
+    } else {
+      console.log("hello");
+      let userObj = {
+        firstName: fn,
+        lastName: ln,
+        email: em,
+        password: pw,
+      };
+
+      MODEL.setUserInfo(userObj);
+    }
+  });
 }
 
 $(document).ready(function () {
-    initURLListener();
+  initURLListener();
 });
--- app/model.js
+++ app/model.js
@@ -1,13 +1,23 @@
 export function changePage(pageID, callback) {
-    if (pageID == '' || pageID == "home") {
-        $.get(`pages/home.html`, function (contents) {
-            $('#app').html(contents);
-            console.log(pageID);
-        });
-    } else {
-        $.get(`pages/${pageID}.html`, function (contents) {
-            $('#app').html(contents);
-            console.log(pageID);
-        });
-    }
+  if (pageID == "" || pageID == "home") {
+    $.get(`pages/home.html`, function (contents) {
+      $("#app").html(contents);
+      console.log(pageID);
+    });
+  } else if (pageID == "login") {
+    $.get(`pages/login.html`, function (data) {
+      $("#app").html(data);
+      callback();
+    });
+  } else {
+    $.get(`pages/${pageID}.html`, function (contents) {
+      $("#app").html(contents);
+      console.log(pageID);
+    });
+  }
+}
+
+export function setUserInfo(userObject) {
+  userInfo = userObject;
+  console.log(userInfo);
 }
